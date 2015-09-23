052f1c748e9c2ff31d6f0f7662455d0c8ed627de
diversant <diversant@corp.devoffice.com>
2015-09-23 12:33:23 +0300

    New documentation engine

---
+++ sections/edit_template/en/edit_psd.php
@@ -0,0 +1,14 @@
+<h3>PSD files editing</h3>
+
+<p>PSD file are located at<b>"sources/psd"</b> PSD file are source ones for template design.</p>
+
+<p>He consist of multiple layers, that's why any of template parts can be changed according to your needs.</p>
+
+<p></p>
+
+<p class="alert alert-warning">Please note these changes won't affect live website unless .html file is edited
+    accordingly.</p>
+
+<p class="alert alert-info">Feel free to check the
+    <a href="/help/general-tutorials/work-with-photoshop/">tutorials on using Photoshop</a> for
+    more detailed explanation.</p>
