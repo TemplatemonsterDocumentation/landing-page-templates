53cadfdbbbb6da323fbee7003b4da88f27c85897
Kholiavko Roman <wildklas@gmail.com>
2016-02-04 19:48:29 +0200

    Add builder doc

---
+++ sections/builder/en/__description.php
@@ -0,0 +1,3 @@
+<h2 class="item3">HTML 5 Builder</h2>
+
+<p>Take the advantages of the HTML 5 Builder tool included with your landing template. Create and customize your layouts and content. Check out <a href="http://builder.chillyorange.com/doc/">the official HTML 5 Builder documentation</a> for further instructions.</p>
