a0ce8a6af5ac92f0e6aca8a9a66f1eaab0053ae1
Kholiavko Roman <wildklas@gmail.com>
2016-02-05 11:39:03 +0200

    Add screenshot to builder section

--- sections/builder/en/__description.php
+++ sections/builder/en/__description.php
@@ -1,3 +1,5 @@
 <h2 class="item3">HTML 5 Builder</h2>
 
 <p>Take the advantages of the HTML 5 Builder tool included with your landing template. Create and customize your layouts and content. Check out <a href="http://builder.chillyorange.com/doc/">the official HTML 5 Builder documentation</a> for further instructions.</p>
+
+<img src="img/builder.jpg" alt="">
--- sections/builder/ru/__description.php
+++ sections/builder/ru/__description.php
@@ -1,3 +1,5 @@
 <h2 class="item3">HTML 5 Билдер</h2>
 
 <p>Ощутите все приемущества HTML 5 билдера входящего в Ваш landing template. Создвавайте и настраивайте ваш шаблон и контент. Воспользуйтесь <a href="http://builder.chillyorange.com/doc/">оффициальной документацией по HTML 5 билдеру</a> для дальнейших инструкций.</p>
+
+<img src="img/builder.jpg" alt="">
