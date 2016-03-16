f83d1f97aca08fb3a15ad8d2883bd7b40df40168
sashko94 <sashko@templatemonster.me>
2016-03-16 11:51:14 +0200

    Update text in sections

--- sections/edit_template/en/edit_html.php
+++ sections/edit_template/en/edit_html.php
@@ -1,13 +1,13 @@
 <h3>.html and .css files editing</h3>
 
-<p>Editable HTML file is located at <b>"site"</b> folder template.</p>
+<p>Editable HTML file is located in the <b>"site"</b> folder of the template</p>
+<p>Sublime Text 3 is an advised application, for HTML file contents editing, but you may also use any other suitable
+    software.</p>
+<p>In order to open .html file with Sublime Text 3 software, click on the file icon with right mouse button and
+    select<b>Open with > Sublime Text 3</b> in the dropdown menu. After file opening you can edit it.</p>
 
-<p>Sublime Text 3 is an advised application, for HTML file contents editing, but you may also use any other suitable software.</p>
-
-<p>In order to open .html file with Sublime Text 3 software, click with right mouse button on file icon and select <b>Open
-        with > Sublime Text</b> 3 in dropdown menu. After file opening you can edit it.</p>
-
-<p>CSS files are used to change template appearance, i.e., fonts (font-family, size, font-face, color, etc.), background, blocks width etc. CSS files can be edited with Sublime Text 3 software. </p>
+<p>CSS files are used to change template appearance, i.e., fonts (font-family, size, font-face, color, etc.),
+    background, blocks width etc. CSS files can be edited with Sublime Text 3 software. </p>
 
 <p class="alert alert-info">At our online support center you may find detailed tutorials on
     <a href="/help/general-tutorials/work-with-css/"> how to edit and manage .css files.</a></p>
